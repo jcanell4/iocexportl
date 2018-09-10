5b89b7bc3894a64129fbd4835a1c3984b6567085
Xavier García <xaviergaro.dev@gmail.com>
2018-09-10 13:37:53 +0200

    Added project info to responses

---
+++ syntax/iocfield.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * lang Syntax Plugin
+ *
+ * @author     Marc Català <mcatala@ioc.cat>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+require_once(DOKU_PLUGIN.'iocexportl/lib/renderlib.php');
+
+class syntax_plugin_iocexportl_iocfield extends DokuWiki_Syntax_Plugin {
+
+    protected $dataSource = null;
+
+   /**
+    * Get an associative array with plugin info.
+    */
+    function getInfo(){
+        return array(
+            'author' => 'Josep Cañellas',
+            'email'  => 'jcanell4@ioc.cat',
+            'date'   => '2015-10-30',
+            'name'   => 'IOC grave Plugin',
+            'desc'   => 'Plugin to parse grave accents syntax in pdf and html',
+            'url'    => 'http://ioc.gencat.cat/',
+        );
+    }
+
+    function getType(){ return 'substition'; }
+    function getPType(){ return 'normal'; }
+    //'container','substition','protected','disabled','baseonly','formatting','paragraphs'
+//    function getAllowedTypes() {
+//        return array('formatting', 'protected');
+//    }
+    function getSort(){
+        return 40;
+    }
+
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+//        $this->Lexer->addSpecialPattern('\x60|\$\\\grave{\\\\:}\$', $mode, 'plugin_iocexportl_iocfield');
+        //$this->Lexer->addSpecialPattern('\{\#\#(?=.*)\#\#\}', $mode, 'plugin_iocexportl_iocfield');
+        $this->Lexer->addSpecialPattern('{##.*?##}', $mode, 'plugin_iocexportl_iocfield');
+//        $this->Lexer->addSpecialPattern('{##durada##}', $mode, 'plugin_iocexportl_iocfield');
+    }
+    
+    /**
+     * Handle the match
+     */
+
+    function handle($match, $state, $pos, &$handler){
+        // $state es un nombre, en aquest cas no ens interessa
+        // $match es la coincidencia, per exemple: {##tipusModulBloc##}
+        // auquests valors arriban com a index 0 = $state y 1 = $match al $data del render
+
+        return array($state, $match);
+    }
+
+   /**
+    * output
+    */
+    function render($mode, &$renderer, $data) {
+
+        $field = substr($data[1], 3, strlen($data[1])-6);
+
+
+        $data = $this->getDataSource();
+
+        $renderer->doc .=  '<b>TODO: obtenir el valor del mdpr per "' . $field . '"</b>';
+
+
+
+        return true;
+
+//        global $symbols;
+//        if ($mode === 'ioccounter'){
+//            $renderer->doc .=  '`';
+//            return TRUE;
+//        }elseif ($mode === 'xhtml'){
+//            $renderer->doc .= '`';
+//            return TRUE;
+//        }elseif ($mode === 'iocxhtml'){
+//            $renderer->doc .= '`';
+//            return TRUE;
+//        }elseif ($mode === 'iocexportl'){
+//            $renderer->doc .= filter_tex_sanitize_formula("$\grave{\:}$");
+//            return TRUE;
+//        }
+//        return FALSE;
+    }
+
+    function setDataSource($dataSource) {
+        $this->dataSource = $dataSource;
+    }
+
+    function getDataSource(){
+        global $plugin_controller;
+
+        if ($this->dataSource) {
+            return $this->dataSource;
+        }
+        else {
+
+            return $plugin_controller->getCurrentProjectDataSource();
+        }
+
+    }
+}
