ab812ca2d82e08e77347c2e61348e94bdd88b57d
Xavier García <xaviergaro.dev@gmail.com>
2018-10-09 14:07:40 +0200

    Updated foreach

--- wioccl/WiocclForEach.php
+++ wioccl/WiocclForEach.php
@@ -1,30 +1,24 @@
 <?php
 require_once "WiocclParser.php";
 
-class WiocclForEach extends WiocclParser {
+class WiocclForEach extends WiocclParser
+{
 
     protected $varName;
-    protected $fullArray =[];
-    protected $filterArgs;
+    protected $fullArray = [];
 
-    const ARG1 = 0;
-    const ARG2 = 2;
-    const OPERATOR = 1;
+    protected $validator;
 
     public function __construct($value = null, $arrays = [], $dataSource)
     {
         parent::__construct($value, $arrays, $dataSource);
 
-        // varName correspón a la propietat var i es el nom de l'array
-        // ALERTA! els arrays es llegeixen com un camp, la conversió d'array al seu valor es tracta al field
-
         $this->varName = $this->extractVarName($value);
         $this->fullArray = $this->extractArray($value);
-        $this->filterArgs = $this->extractFilterArgs($value);
-
+        $this->validator = new _WiocclCondition($value, $arrays, $dataSource);
     }
 
-    protected function parseTokens($tokens, &$tokenIndex)
+    protected function parseTokens($tokens, &$tokenIndex = 0)
     {
 
         $result = '';
@@ -32,26 +26,11 @@ class WiocclForEach extends WiocclParser {
         $lastBlockIndex = null;
         $lastTokenIndex = 0;
 
-
-        for ($arrayIndex = 0; $arrayIndex<count($this->fullArray); $arrayIndex++) {
+        for ($arrayIndex = 0; $arrayIndex < count($this->fullArray); $arrayIndex++) {
 
             $tokenIndex = $startTokenIndex;
             $row = $this->fullArray[$arrayIndex];
             $this->arrays[$this->varName] = $row;
-
-
-            // TODO: Extreure a una funció a part per poder reutilizar al foreach, if i subset
-
-            $arg1 = $this->normalizeArg((new WiocclParser($this->filterArgs[self::ARG1], $this->arrays, $this->dataSource))->getValue());
-            $arg2 = $this->normalizeArg((new WiocclParser($this->filterArgs[self::ARG2], $this->arrays, $this->dataSource))->getValue());
-
-
-
-
-
-
-
-
 
             // Els format dels arrays al fullArray es similar a (sense els index 0, 1, etc):
 //            '[
@@ -62,18 +41,16 @@ class WiocclForEach extends WiocclParser {
             // El format que es passa a arrays es:
             //      ['tipus' => 'lalala', 'eina' => 'ggg', 'opcionalitat' => 'cap']
 
+            $process = $this->validator->validate();
 
-            // ALERTA: No es pot fer
-            $process = $this->filterArgs==NULL || $this->resolveCondition($arg1, $arg2, $this->filterArgs[self::OPERATOR]);
-
-            if (!$process && $lastTokenIndex >0) {
+            if (!$process && $lastTokenIndex > 0) {
                 // Ja s'ha processat previament el token de tancament i no s'acompleix la condició, no cal continuar processant
                 continue;
             }
 
             while ($tokenIndex < count($tokens)) {
 
-                $parsedValue =  $this->parseToken($tokens, $tokenIndex);
+                $parsedValue = $this->parseToken($tokens, $tokenIndex);
 
                 if ($parsedValue === null) { // tancament del foreach
                     break;
@@ -91,56 +68,6 @@ class WiocclForEach extends WiocclParser {
 
         $tokenIndex = $lastTokenIndex;
 
-
         return $result;
-    }
-
-
-    //** ALERTA! Duplicat al  foreach*/
-    protected function extractFilterArgs($value) {
-        if (preg_match('/filter="(.*?([><=]=?))(.*?)">/', $value, $matches) === 1) {
-            // ALERTA: Actualment el token amb > arriba tallat perquè l'identifica com a tancament del token d'apertura
-
-            $arg1 = $this->normalizeArg(str_replace(['==', '>', '<', '=', '>=', '<=', '!='], '', $matches[1]));
-            $arg2 = $matches[3];
-
-            $operator = $matches[2];
-
-
-
-            return [$arg1, $operator, $arg2];
-//            throw new Exception("Incorrect condition structure");
-        };
-
-
-
-
-        return null;
-    }
-
-
-    // Duplict al foreach, subset i el if
-    protected function resolveCondition($arg1, $arg2, $operator)
-    {
-
-        switch ($operator) {
-
-            case '==':
-                return $arg1 == $arg2;
-            case '<=':
-                return $arg1 <= $arg2;
-            case '<':
-                return $arg1 < $arg2;
-            case '>=':
-                return $arg1 >= $arg2;
-            case '>':
-                return $arg1 > $arg2;
-            case '!=':
-                return $arg1 != $arg2;
-
-            default:
-                return $arg1 && $arg2;
-        }
-
     }
 }
