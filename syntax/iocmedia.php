9446a717c6443098b098622b0f85e4da76c4eb52
Xavier García <xaviergaro.dev@gmail.com>
2020-05-20 12:57:42 +0200

    Updated iocmedia to use SharedConstants

--- syntax/iocmedia.php
+++ syntax/iocmedia.php
@@ -4,16 +4,21 @@
  * @author     Marc Català <mcatala@ioc.cat>
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
-if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-if (!defined('DOKU_PLUGIN_TEMPLATES')) define('DOKU_PLUGIN_TEMPLATES',DOKU_PLUGIN.'iocexportl/templates/');
-require_once(DOKU_PLUGIN.'syntax.php');
-require_once(DOKU_PLUGIN.'iocexportl/lib/renderlib.php');
+if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+if (!defined('DOKU_PLUGIN_TEMPLATES')) define('DOKU_PLUGIN_TEMPLATES', DOKU_PLUGIN . 'iocexportl/templates/');
+require_once(DOKU_PLUGIN . 'syntax.php');
+require_once(DOKU_PLUGIN . 'iocexportl/lib/renderlib.php');
+
+if (!defined('DOKU_LIB_IOC')) define('DOKU_LIB_IOC', DOKU_INC . "lib/lib_ioc/");
+require_once DOKU_LIB_IOC . "common/SharedConstants.php";
+
 
 class syntax_plugin_iocexportl_iocmedia extends DokuWiki_Syntax_Plugin {
-    static $vimeo = 'https://player.vimeo.com/video/@VIDEO@';
-    static $youtube = 'https://www.youtube.com/embed/@VIDEO@?controls=1';
-    static $dailymotion = 'https://www.dailymotion.com/embed/video/@VIDEO@';
+//    static $vimeo = 'https://player.vimeo.com/video/@VIDEO@';
+//    static $youtube = 'https://www.youtube.com/embed/@VIDEO@?controls=1';
+//    static $dailymotion = 'https://www.dailymotion.com/embed/video/@VIDEO@';
+
     static $altamarFromUrl = 'vídeo[altamar: @VIDEO@]';
     static $altamarFromId = 'vídeo[altamar: @VIDEO@]';
     static $altamarFromReq = 'vídeo[altamar: @VIDEO@]';
@@ -21,29 +26,29 @@ class syntax_plugin_iocexportl_iocmedia extends DokuWiki_Syntax_Plugin {
 //    static $altamarFromUrl = 'http://bcove.me/@VIDEO@';
 //    static $altamarFromId = http://link.brightcove.com/services/player/bcpid1326284612001?bckey=AQ~~,AAABNMyTcTE~,zjiPB9Bfp4EykEGoTnvDHUfnwtGu2QvJ&bctid=@VIDEO@';
 
-   /**
-    * Get an associative array with plugin info.
-    */
-    function getInfo(){
+    /**
+     * Get an associative array with plugin info.
+     */
+    function getInfo() {
         return array(
             'author' => 'Marc Català',
-            'email'  => 'mcatala@ioc.cat',
-            'date'   => '2011-01-27',
-            'name'   => 'IOC media Plugin',
-            'desc'   => 'Plugin to parse media files',
-            'url'    => 'http://ioc.gencat.cat/',
+            'email' => 'mcatala@ioc.cat',
+            'date' => '2011-01-27',
+            'name' => 'IOC media Plugin',
+            'desc' => 'Plugin to parse media files',
+            'url' => 'http://ioc.gencat.cat/',
         );
     }
 
-    function getType(){
+    function getType() {
         return 'substition';
     }
 
-    function getPType(){
+    function getPType() {
         return 'block';
     } //stack, block, normal
 
-    function getSort(){
+    function getSort() {
         return 318; //{{uri}} dokuwiki has 320 priority
     }
 
@@ -57,80 +62,107 @@ class syntax_plugin_iocexportl_iocmedia extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, &$handler) {
         // remove {{ }}
-        $command = substr($match,2,-2);
+        $command = substr($match, 2, -2);
 
         // title
-        list($command, $title) = explode('|',$command);
+        list($command, $title) = explode('|', $command);
         $title = trim($title);
         $command = trim($command);
 
         // get site and video
-        list($site,$url) = explode('>',$command);
+        list($site, $url) = explode('>', $command);
 
         // what size?
-        list($url,$param) = explode('?',$url,2);
-        if(preg_match('/(\d+)x(\d+)/i',$param,$m)){
+        $width = '';
+        $height = '';
+
+        list($url, $param) = explode('?', $url, 2);
+        if (preg_match('/(\d+)x(\d+)/i', $param, $m)) {
             // custom
-            $width  = $m[1];
+            $width = $m[1];
             $height = $m[2];
-        }elseif(strpos($param,'small') !== false){
-            // small
-            $width  = 255;
-            $height = 210;
-        }elseif(strpos($param,'large') !== false){
-            // large
-            $width  = 520;
-            $height = 406;
-        }else{
-            // medium
-            $width  = 425;
-            $height = 350;
+        } else {
+
+            foreach (SharedConstants::ONLINE_VIDEO_CONFIG['sizes'] as $key => $value) {
+
+                if (strpos($param, $key) !== false) {
+                    list($width, $height) = explode('x', $value, 2);
+                    break;
+                }
+            }
+
         }
+
+
+//        }elseif(strpos($param,'small') !== false){
+//            // small
+//            $width  = 255;
+//            $height = 210;
+//        }elseif(strpos($param,'large') !== false){
+//            // large
+//            $width  = 520;
+//            $height = 406;
+//        }else{
+//            // medium
+//            $width  = 425;
+//            $height = 350;
+//        }
 
         return array($site, $url, $title, $width, $height);
     }
 
-   /**
-    * output
-    */
+    /**
+     * output
+     */
     function render($mode, &$renderer, $data) {
-        if ($mode == 'wikiiocmodel_psdom'){
+        if ($mode == 'wikiiocmodel_psdom') {
             $url = $this->generaURL($data);
             list(, , $title) = $data;
             $text = "$url | $title";
             $renderer->getCurrentNode()->addContent(new TextNodeDoc(TextNodeDoc::PLAIN_TEXT_TYPE, $text));
             return TRUE;
-        }elseif ($mode === 'iocexportl'){
+        } elseif ($mode === 'iocexportl') {
             $this->generaQRCode($renderer, $data);
             return TRUE;
-        }elseif ($mode === 'ioccounter'){
+        } elseif ($mode === 'ioccounter') {
             list($site, $url, $title) = $data;
             $renderer->doc .= $title;
-        }elseif (strpos("xhtml/iocxhtml/wikiiocmodel_ptxhtml", $mode) !== FALSE){
+        } elseif (strpos("xhtml/iocxhtml/wikiiocmodel_ptxhtml", $mode) !== FALSE) {
             list($site, $params, $title, $width, $height) = $data;
-            if($site === 'dailymotion' || $site === 'vimeo'
-                    || $site === 'youtube'){
-                if ($site === 'dailymotion'){
-                    $type = self::$dailymotion;
-                }elseif($site === 'vimeo'){
-                    $type = self::$vimeo;
-                }else{
-                    $type = self::$youtube;
-                }
+
+
+            if (array_key_exists($site, SharedConstants::ONLINE_VIDEO_CONFIG['origins'])) {
+                $type = SharedConstants::ONLINE_VIDEO_CONFIG['origins'][$site]['url_template'];
                 list($url, $full) = explode(":", $params);
-                $url = preg_replace('/@VIDEO@/', $url, $type);
+                $url = preg_replace('/\$\{id\}/', $url, $type);
             }
+
+
+//            if($site === 'dailymotion' || $site === 'vimeo'
+//                    || $site === 'youtube'){
+//                if ($site === 'dailymotion'){
+//                    $type = self::$dailymotion;
+//                }elseif($site === 'vimeo'){
+//                    $type = self::$vimeo;
+//                }else{
+//                    $type = self::$youtube;
+//                }
+//                list($url, $full) = explode(":", $params);
+//                $url = preg_replace('/@VIDEO@/', $url, $type);
+//            }
             $renderer->doc .= '<div class="mediavideo">';
+
+
             if ($site === 'altamarVideos') {
-                $tpl = io_readFile(DOKU_PLUGIN_TEMPLATES.$site.'.tpl');
+                $tpl = io_readFile(DOKU_PLUGIN_TEMPLATES . $site . '.tpl');
                 $tpl = preg_replace("/@HEIGHT@/", $height, $tpl);
                 $tpl = preg_replace("/@WIDTH@/", $width, $tpl);
                 $tpl = preg_replace("/@ID_VIDEO@/", strval($params), $tpl);
                 $renderer->doc .= $tpl;
-            }else{
-                $renderer->doc .='<iframe height="'.$height.'px" width="'.$width.'px" src="'.$url.'"></iframe>';
+            } else {
+                $renderer->doc .= '<iframe height="' . $height . 'px" width="' . $width . 'px" src="' . $url . '"></iframe>';
                 $tagMessage = WikiIocLangManager::getLang("showInFullScreen", "iocexportl");
                 $renderer->doc .= "<p><a target=_blank href='$url'>$tagMessage</a></p>";
             }
@@ -142,19 +174,33 @@ class syntax_plugin_iocexportl_iocmedia extends DokuWiki_Syntax_Plugin {
     private function generaURL($data) {
         list($site, $params) = $data;
 
-        if ($site === 'dailymotion') {
-            $type = self::$dailymotion;
-        }elseif ($site === 'vimeo') {
-            $type = self::$vimeo;
-        }elseif ($site === 'altamarVideos') {
+        list($url) = explode(":", $params);
+
+        if ($site !== 'altamarVideos' && array_key_exists($site, SharedConstants::ONLINE_VIDEO_CONFIG['origins'])) {
+            $type = SharedConstants::ONLINE_VIDEO_CONFIG['origins'][$site]['url_template'];
+            $url = preg_replace('/\$\{id\}/', $url, $type);
+        } else if ($site === 'altamarVideos') {
             $type = self::$altamarVideos;
-        }elseif ($site === 'youtube') {
-            $type = self::$youtube;
-        }
-        if ($type) {
-            list($url) = explode(":", $params);
             $url = preg_replace('/@VIDEO@/', $url, $type);
         }
+
+//        if ($site === 'dailymotion') {
+//            $type = self::$dailymotion;
+//        }elseif ($site === 'vimeo') {
+//            $type = self::$vimeo;
+//        }elseif ($site === 'altamarVideos') {
+//            $type = self::$altamarVideos;
+//        }elseif ($site === 'youtube') {
+//            $type = self::$youtube;
+//        }
+
+
+//
+//        if ($type) {
+//            list($url) = explode(":", $params);
+//            $url = preg_replace('/@VIDEO@/', $url, $type);
+//        }
+
         return $url;
     }
 
