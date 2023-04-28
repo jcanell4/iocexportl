1a6ac939d698ed6da98227cdfeade618837ce865
rclaver <rclaver@xtec.cat>
2023-04-28 13:44:22 +0200

    correcció title gif

--- syntax/iocgif.php
+++ syntax/iocgif.php
@@ -93,6 +93,7 @@ class syntax_plugin_iocexportl_iocgif extends DokuWiki_Syntax_Plugin {
             $href = "$rUrl$rDir/web/${rDir}htmlindex/gifs/".str_replace(":", "/", $ns)."/$gif";
 
             $_SESSION['gif_images'] = $href;
+            $title = (empty($title)) ? $gif : $title;
             qrcode_media_url($renderer, $href, $title, $type);
             return TRUE;
 
